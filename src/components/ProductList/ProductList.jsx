b4a6db2cf489c7b846fb7e50204e20197d7fc238
vmay <va>
2024-05-25 17:19:15 +0300

    fdsfsds

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -31,7 +31,7 @@ const ProductList = () => {
             totalPrice: getTotalPrice(addedItems),
             queryId,
         }
-        fetch('http://192.168.0.79:8000/web-data', {
+        fetch('http://45.89.188.162:8000/web-data', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
